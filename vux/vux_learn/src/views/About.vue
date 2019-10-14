d8774606a50b58152f45949a3ef26e4982dbd8eb
李吴奇 <liwuqi@liwuqideMacBook-Pro.local>
2019-10-14 16:11:08 +0800

    学习flex.css

--- vux/vux_learn/src/views/About.vue
+++ vux/vux_learn/src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="content-wrapper" flex="main:center cross:stretch">
+      <div class="content" style="width: 100px;">1</div>
+      <div class="content" flex-box="1">2</div>
+      <div class="content" flex-box="1">3</div>
+      <div class="content" flex-box="1">4</div>
+      <div class="content" flex-box="2">5</div>
+    </div>
   </div>
 </template>
+<script>
+import 'flex.css';
+export default {
+  
+}
+</script>
+<style lang="css">
+  .content-wrapper {
+    width: 500px;
+    height: 500px;
+    border: 1px solid #ccc;
+    margin: 0 auto;
+  }
+  .content {
+    width: 50px;
+    height: 50px;
+    background: #99f;
+  }
+</style>
